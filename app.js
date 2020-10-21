88a637660b6fc24b4dd035b639e38ca3ebaa6296
Christina Poole <codenamedchris@gmail.com>
2020-10-20 21:41:38 -0400

    First commit

--- app.js
+++ app.js
@@ -1,58 +1,144 @@
-/**
- * Example store structure
- */
+/* eslint-disable strict */
 const store = {
-  // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
-      answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
-      ],
-      correctAnswer: 'green'
+      question: 'Who was the first "coder"?',
+      answers: ["Ada Lovelace", "Al Gore", "Albert Einstien", "John Backus"],
+      correctAnswer: "Ada Lovelace",
     },
     {
-      question: 'What is the current year?',
-      answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
-      ],
-      correctAnswer: '2019'
-    }
+      question: "When was the first computer virus created?",
+      answers: ["1994", "1983", "1956", "1899"],
+      correctAnswer: "1983",
+    },
+    {
+      question: "When was the first computer game created?",
+      answers: ["1973", "1994", "2003", "1961"],
+      correctAnswer: "1961",
+    },
+    {
+      question: "What was the first programming language?",
+      answers: ["Java", "Java Script", "C#", "Fortran"],
+      correctAnswer: "Fortran",
+    },
+    {
+      question: 'When was the first known usage of the word "computer"?',
+      answers: ["1921", "1897", "1613", "1952"],
+      correctAnswer: "1613",
+    },
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material and access support for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
+//page shows start button
+function startPage() {
+  let startPage = `
+  <section class ='card'>
+  <h2 class="center">Welcome to my Quiz!</h2>
+  <p class="center">Take this quiz to test your coding knowledge.</p>
+  <button class="center" id="start">Start Quiz</button>
+  </section>`;
+  return startPage;
+}
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+//start button yields question page via render()
+function handleStartQuiz() {
+  $('main').on('click', '#start', function () {
+    store.quizStarted = true;
+    render();
+  });
+}
 
-// These functions return HTML templates
+//page shows question/answer choices, submit button...
+function questionPage() {
+  let question = store.questions[store.questionNumber]; //yields entire questions array object
 
-/********** RENDER FUNCTION(S) **********/
+  let questionPage = `
+    <section class="card">
+    <h2 class="center">${question.question}</h2>
+    <ul class="metric">
+    <li>Question ${store.questionNumber + 1} out of ${store.questions.length}</li>
+    <li>Current score: ${store.score}</li>
+    </ul>
+    <form class="-"> 
+      <label>${question.answers[0]}</label>
+      <input type="radio" name="answer" value="${question.answers[0]}"
+      <label>${question.answers[1]}</label>
+      <input type="radio" name="answer" value="${question.answers[1]}"
+      <label>${question.answers[2]}</label>
+      <input type="radio" name="answer" value="${question.answers[2]}"
+      <label>${question.answers[3]}</label>
+      <input type="radio" name="answer" value="${question.answers[3]}"
+      </form>
+      <button class="submit">Submit</button>
+    </section>`; //shouldnt hardcode
+  return questionPage;
+}
 
-// This function conditionally replaces the contents of the <main> tag based on the state of the store
+//submit yields feedback page
+function handleAnswerSubmit() {
+  $('input').on('click', 'input value', function () {
+    const userAnswer = $(event.currentTarget).find('input value');
+    console.log(userAnswer);
+  });
+  $('main').on('click', '#submit', function () {
+    event.preventDefault();
+    $(".js-display-user-text").text(`user text is:  ${userTextElement.val()}`);
+    userTextElement;
+    // const score =;
+    render();
+  });
+}
 
-/********** EVENT HANDLER FUNCTIONS **********/
+//page shows correct answer if 
+//wrong answer chosen & feedback
+function feedbackPage() {
+  let feedbackPage = `
+  `;
+  return feedbackPage;
+}
 
-// These functions handle events (submit, click, etc)
+function handleFeedbackPage() {
+//next button yields question page
+}
+
+function render() {
+  if (store.quizStarted === false) {
+    $('main').html(startPage);
+  } else if (store.quizStarted === true) {
+    $('main').html(questionPage()); //yields question page
+    store.questionNumber++; //go to next question 
+    
+    handleAnswerSubmit();
+  }
+}
+
+function main() {
+  render();
+  startPage();
+  handleStartQuiz();
+}
+
+$(main);
+
+
+
+
+
+
+
+
+
+
+// function renderStartPage(){
+//   if(store.quizStarted === false){
+//     $('main').html(startPage);
+//   }
+  
+//   function renderQuizPage(){
+//     if(store.quizStarted === true){
+//     $('main').html(quizPage);
+//     }
+//   }
